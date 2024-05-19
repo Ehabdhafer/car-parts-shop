62e99c03f9180df96d39fc195f03dc01c5f72b74
unknown <ehabzaferabed@gmail.com>
2024-05-19 19:11:20 +0300

    add signup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue": "#344357",
+        "custom-red": "#e20201",
+      },
+    },
   },
   plugins: [],
 };
