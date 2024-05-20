b9f770ef718a409ca91828a1207f445fdc6448b8
unknown <ehabzaferabed@gmail.com>
2024-05-20 15:07:29 +0300

    edit login functions

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -37,4 +37,26 @@ class UserController extends Controller
             return response()->json(['error' => 'Internal Server Error', 'message' => $e->getMessage()], 500);
         }
     }
+
+    public function login(Request $req)
+    {
+        try {
+            $req->validate([
+                'email' => 'required|max:30|min:10|email',
+                'password' => 'required|max:40|min:6',
+            ]);
+            $user = User::checkemail($req->email);
+            if (!$user || !User::checkpass($req->password, $user->password)) {
+                return response()->json(['message' => 'Invalid Email or Password'], 400);
+            }
+
+            $user->tokens->each->delete();
+            $token = $user->createtoken('token', ['*'], now()->addHours(6))->plainTextToken;
+
+            return response()->json(['message' => 'Loggedin Successfully', 'token' => $token], 200);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(['error' => 'Internal Server Error', 'message' => $e->getMessage()], 500);
+        }
+    }
 }
