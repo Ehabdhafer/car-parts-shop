156bf48e310a29248834d5f13477b7ab3d59b5d5
unknown <ehabzaferabed@gmail.com>
2024-05-21 15:41:55 +0300

    edit header ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./hooks/Authcontext";
 import Signup from "./components/signup.jsx";
 import Login from "./components/login.jsx";
+import Header from "./components/header.jsx";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <div className="App">
+          {window.location.pathname !== "/login" &&
+          window.location.pathname !== "/signup" ? (
+            <Header />
+          ) : (
+            <></>
+          )}
           <Routes>
             {/* <Route exact path="/" element={<Home />} /> */}
             <Route exact path="/login" element={<Login />} />
---
+++ client/src/components/header.jsx
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <div>home</div>
+        <div>View our large assortment of more than 2,500 spare parts</div>
+      </div>
+
+      <div>
+        <div>
+          search
+          <button>search</button>
+        </div>
+        <div>
+          <img src="" alt="profile" />
+          <div>Signin / Signup</div>
+        </div>
+        <div>
+          <img src="" alt="cart" />
+          <div>$ price(0.00)</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
